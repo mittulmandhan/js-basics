abe62589cddc3d9e25effddcc7f6f608b74356e8
mittulmandhan <conanmittul@gmail.com>
2019-10-01 12:34:32 +0530

    non-typed language concept

---
+++ variable.js
@@ -0,0 +1,22 @@
+// non-typed language
+
+var x;
+console.log(x); //undefined
+
+var x = 5;
+console.log(x); //5
+
+var x;
+console.log(x); //5
+
+var x = 'Hi';
+console.log(x); //Hi
+
+x = true;
+console.log(x); //Hi
+
+x = { name: 'mitta' };
+console.log(x);
+
+x = [12, 23, 45];
+console.log(x);
