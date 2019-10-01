779df59d0feedaf9e866fbb6bc4f94e8510779c7
mittulmandhan <conanmittul@gmail.com>
2019-10-01 12:33:29 +0530

    declaring a variable

---
+++ let-var-const.js
@@ -0,0 +1,13 @@
+if (true) {
+    //ES5 function scope
+    var x = 4;
+
+    //ES6 block level
+    let y = 3;
+    const z = 3.14;
+
+    //z = 4; //error
+}
+console.log(x);
+//console.log(y);
+//console.log(z);
