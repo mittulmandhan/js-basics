b3bb59a3c283e1dff25c58cb6063ccb399ab6174
mittulmandhan <conanmittul@gmail.com>
2019-10-01 12:39:25 +0530

    typeof operator in js

---
+++ typeof.js
@@ -0,0 +1,7 @@
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
+console.log(typeof(Infinity)); //number
+console.log(typeof(NaN)); //number
+console.log(typeof('null')); //string
+console.log(typeof(12)); //number
+console.log(typeof(true)); //boolean
